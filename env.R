48708b4a11d4a750e9073be01ac67ab535a478f1
AlexLietard <alex.lietard77@gmail.com>
2026-06-05 14:38:19 +0200

    first commit

---
+++ env.R
@@ -0,0 +1,8 @@
+# Just for the tutorial
+install.packages("renv")
+# renv::init()
+# install.packages("ggplot2")
+# install.packages("dplyr")
+# install.packages("rmarkdown")
+# install.packages("knitr")
+renv::snapshot()
